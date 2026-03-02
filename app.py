0a3ff716852a8196b60d3b9c65fbfd4d4244a285
mcdavies4 <azubuikedavies@gmail.com>
2026-03-02 23:19:35 +0000

    Remove payments import

--- app.py
+++ app.py
@@ -176,8 +176,6 @@ def lookup_location(query):
     return None, None, None
 
 # Register payment routes
-from payments import register_payment_routes
-register_payment_routes(app)
 
 @app.route('/')
 def index():
