282840aa4ed41b9c955dc00d014823eb8e9988cb
mcdavies4 <azubuikedavies@gmail.com>
2026-03-02 19:31:45 +0000

    Clean app.py - fix merge conflict

--- app.py
+++ app.py
@@ -279,8 +279,4 @@ def robots():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-<<<<<<< HEAD
     app.run(host='0.0.0.0', port=port, debug=False)
-=======
-    app.run(host='0.0.0.0', port=port, debug=False)
->>>>>>> 4a4569d15869306ab908ca80a3174a02affad566
